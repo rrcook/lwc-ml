81c67937246d43c8c8897a91a410949a2e7df090
Richard Cook <blubparadox@gmail.com>
2020-07-16 21:41:56 -0400

    Added ML5 to TF.

--- force-app/main/default/lwc/tfPlayground/tfPlayground.js
+++ force-app/main/default/lwc/tfPlayground/tfPlayground.js
@@ -2,20 +2,34 @@ import { LightningElement, api, track } from 'lwc';
 
 export default class TfPlayground extends LightningElement {
     tfVersion;
+    ml5Version;
 
-    handleGetVersionClick() {
-        console.log('In Get Version Click');
-        const getVersionEvent = new CustomEvent('getversion', {
+    handleGetTfVersionClick() {
+        console.log('In Get TF Version Click');
+        const getVersionEvent = new CustomEvent('gettfversion', {
             detail: { message: 'hello' }
         });
         this.dispatchEvent(getVersionEvent);
+    }
 
+    handleGetMl5VersionClick() {
+        console.log('In Get ML5 Version Click');
+        const getVersionEvent = new CustomEvent('getml5version', {
+            detail: { message: 'hello' }
+        });
+        this.dispatchEvent(getVersionEvent);
     }
 
     @api
-    receiveVersion(version) {
-        console.log(`In receiveVersion, ${version}`);
+    returnTfVersion(version) {
+        console.log(`In returnTfVersion, ${version}`);
         this.tfVersion = version;
+    }
+
+    @api
+    returnMl5Version(version) {
+        console.log(`In returnMl5Version, ${version}`);
+        this.ml5Version = version;
     }
 
 }
