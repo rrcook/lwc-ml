d47d15e6ca0d1631fe72b68a8fecbc2ae17b4926
Richard Cook <blubparadox@gmail.com>
2020-08-01 22:17:42 -0400

    Aura component hosting TensorFlow. Builds the dense neural network layer, no errors.

--- force-app/main/default/aura/tfHost/tfHostController.js
+++ force-app/main/default/aura/tfHost/tfHostController.js
@@ -1,14 +1,15 @@
 ({
 
-    scriptsLoaded : function(component, event, helper) {
-        console.log('scripts loaded');
+    scriptsLoaded: function(component, event, helper) {
+        console.log('Aura-Scripts loaded.');
+        const playground = component.find('playground');
         let tfVersion = 'N/A';
         try {
             tfVersion = tf.version.tfjs;
         } catch (error) {
             console.log(`Error loading tf, ${error}`);
         }
-        component.find('playground').returnTfVersion(tfVersion);
+        playground.returnTfVersion(tfVersion);
 
         let ml5Version = 'N/A';
         try {
@@ -16,23 +17,29 @@
         } catch (error) {
             console.log(`Error loading ml5, ${error}`);
         }
-        component.find('playground').returnMl5Version(ml5Version);
+        playground.returnMl5Version(ml5Version);
     },
 
-    handleGetTfVersion : function(component, event, helper) {
-        const version = tf ? tf.version.tfjs : 'pending';
-        console.log('aura - handleTfGetVersion, version is ' + version);
+    handleGetTfVersion: function(component, event, helper) {
+        const version = tf ? tf.version.tfjs: 'pending';
+        console.log('Aura - handleTfGetVersion, version is ' + version);
         console.log('message is ' + event.getParam('message'));
         console.log('Trying to find, ' + component.find('playground'));
         component.find('playground').returnTfVersion(version);
     },
 
-    handleGetMl5Version : function(component, event, helper) {
-        const version = ml5 ? ml5.version : 'pending';
+    handleGetMl5Version: function(component, event, helper) {
+        const version = ml5 ? ml5.version: 'pending';
         console.log('aura - handleMl5GetVersion, version is ' + version);
         console.log('message is ' + event.getParam('message'));
         console.log('Trying to find, ' + component.find('playground'));
         component.find('playground').returnMl5Version(version);
+    },
+
+    handleRunBostonHousing: function(component, event, helper) {
+        let eraseme = event.getParam('bostonData');
+        console.log(`controller train features length ${eraseme.trainFeatures.length}`);
+        helper.runBostonHousing(component, event.getParam('bostonData'));
     }
 
 
--- force-app/main/default/aura/tfHost/tfHostHelper.js
+++ force-app/main/default/aura/tfHost/tfHostHelper.js
@@ -1,5 +1,154 @@
 ({
-    helperMethod : function() {
+    // Some hyperparameters for model training.
+    NUM_EPOCHS: 200,
+    BATCH_SIZE: 40,
+    LEARNING_RATE: 0.01,
 
+    bostonData: {},
+    tensors: {},
+    numFeatures: 0,
+
+
+
+    helperMethod: function () {
+
+    },
+
+    runBostonHousing: function (component, bostonData) {
+        console.log(`host in run boston housing`);
+        const playground = component.find('playground');
+        this.bostonData = bostonData;
+        this.numFeatures = Object.keys(bostonData.trainFeatures[0]).length;
+        this.arraysToTensors();
+        playground.updateStatus("Arrays converted to Tensors.");
+
+        const model = this.multiLayerPerceptronRegressionModel2Hidden();
+        console.log(`host model is ${model.toString()}`);
+
+    },
+
+    /**
+     * Builds and returns Multi Layer Perceptron Regression Model
+     * with 2 hidden layers, each with 10 units activated by sigmoid.
+     *
+     * @returns {tf.Sequential} The multi layer perceptron regression mode  l.
+     */
+    multiLayerPerceptronRegressionModel2Hidden: function () {
+        const model = tf.sequential();
+        model.add(tf.layers.dense({
+            inputShape: [this.numFeatures],
+            units: 50,
+            activation: 'sigmoid',
+            kernelInitializer: 'leCunNormal'
+        }));
+        model.add(tf.layers.dense(
+            { units: 50, activation: 'sigmoid', kernelInitializer: 'leCunNormal' }));
+        model.add(tf.layers.dense({ units: 1 }));
+
+        model.summary();
+        return model;
+    },
+
+
+    /**
+     * Describe the current linear weights for a human to read.
+     *
+     * @param {Array} kernel Array of floats of length 12.  One value per feature.
+     * @returns {List} List of objects, each with a string feature name, and value
+     *     feature weight.
+     */
+    describeKernelElements: function (kernel) {
+        tf.util.assert(
+            kernel.length == 12,
+            `kernel must be a array of length 12, got ${kernel.length}`);
+        const outList = [];
+        for (let idx = 0; idx < kernel.length; idx++) {
+            outList.push({ description: featureDescriptions[idx], value: kernel[idx] });
+        }
+        return outList;
+    },
+
+
+
+    // functions to turn Salesforce/JavaScript objects into arrays
+    createArrayFromFeatures: function (item) {
+        return [
+            item.Crime_Rate__c,
+            item.Land_Zone_Size__c,
+            item.Industrial_Proportion__c,
+            item.Next_To_River__c,
+            item.Nitric_Oxide_Concentration__c,
+            item.Rooms_Per_House__c,
+            item.Age_Of_Housing__c,
+            item.Distance_To_Commute__c,
+            item.Distance_To_Highway__c,
+            item.Tax_Rate__c,
+            item.School_Class_Size__c,
+            item.School_Dropout_Rate__c
+        ];
+    },
+
+    createArrayFromTarget: function (item) {
+        return [
+            item.Median_Value__c
+        ];
+    },
+
+    // Convert loaded data into tensors and creates normalized versions of the
+    // features.
+    arraysToTensors: function () {
+        // console.log(`test features is ${JSON.stringify(this.bostonData.testFeatures)}`);
+        // let tester = this.bostonData.testFeatures.map(this.createArrayFromData);
+        // let tester = this.bostonData.testFeatures);
+        // console.log(`host tester is ${JSON.stringify(tester)}`);
+        this.tensors.rawTrainFeatures = tf.tensor2d(this.bostonData.trainFeatures.map(this.createArrayFromFeatures));
+        this.tensors.trainTargets = tf.tensor2d(this.bostonData.trainTargets.map(this.createArrayFromTarget));
+        this.tensors.rawTestFeatures = tf.tensor2d(this.bostonData.testFeatures.map(this.createArrayFromFeatures));
+        this.tensors.testTargets = tf.tensor2d(this.bostonData.testTargets.map(this.createArrayFromTarget));
+        // Normalize mean and standard deviation of data.
+        let { dataMean, dataStd } =
+            this.determineMeanAndStddev(this.tensors.rawTrainFeatures);
+
+        this.tensors.trainFeatures = this.normalizeTensor(
+            this.tensors.rawTrainFeatures, dataMean, dataStd);
+        this.tensors.testFeatures =
+            this.normalizeTensor(this.tensors.rawTestFeatures, dataMean, dataStd);
+    },
+
+
+    // From normalization.js
+    /**
+     * Calculates the mean and standard deviation of each column of a data array.
+     *
+     * @param {Tensor2d} data Dataset from which to calculate the mean and
+     *                        std of each column independently.
+     *
+     * @returns {Object} Contains the mean and standard deviation of each vector
+     *                   column as 1d tensors.
+     */
+    determineMeanAndStddev: function (data) {
+        const dataMean = data.mean(0);
+        // TODO(bileschi): Simplify when and if tf.var / tf.std added to the API.
+        const diffFromMean = data.sub(dataMean);
+        const squaredDiffFromMean = diffFromMean.square();
+        const variance = squaredDiffFromMean.mean(0);
+        const dataStd = variance.sqrt();
+        return { dataMean, dataStd };
+    },
+
+    /**
+     * Given expected mean and standard deviation, normalizes a dataset by
+     * subtracting the mean and dividing by the standard deviation.
+     *
+     * @param {Tensor2d} data: Data to normalize. Shape: [batch, numFeatures].
+     * @param {Tensor1d} dataMean: Expected mean of the data. Shape [numFeatures].
+     * @param {Tensor1d} dataStd: Expected std of the data. Shape [numFeatures]
+     *
+     * @returns {Tensor2d}: Tensor the same shape as data, but each column
+     * normalized to have zero mean and unit standard deviation.
+     */
+    normalizeTensor: function (data, dataMean, dataStd) {
+        return data.sub(dataMean).div(dataStd);
     }
+
 })
